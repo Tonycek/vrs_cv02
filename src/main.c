ac5d86e7ca7c4c98575a9a819aeb8cd626c5593d
Tonycek <tonko.horvath@gmail.com>
2016-09-27 14:20:50 +0200

    Uloha 1-2
    nastavenie led na 1 a 0 pomocou registru odr

--- src/main.c
+++ src/main.c
@@ -73,13 +73,14 @@ int main(void)
   GPIOA->MODER |= (0b01)<<(5*2);
   GPIOA->OTYPER &= ~((uint16_t)(1<<5));
   GPIOA->PUPDR |= (0b01)<<(5*2);
-  GPIOA->ODR |= 0b0000000000100000;
   GPIOA->OSPEEDR |= (0b10)<<(5*2);
 
   /* Infinite loop */
   while (1)
   {
-	i++;
+	  GPIOA->ODR |= ((uint16_t)(1 << 5));
+
+	  GPIOA->ODR &= ~((uint16_t)(1 << 5));;
   }
   return 0;
 }
